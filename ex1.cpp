5f6e0701cb3bd07a761d8fcca36b32c32ef9f112
Kavi-Priya-M <kavipriyamani2023@gmail.com>
2026-03-30 11:42:36 +0530

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string password,name;
+    cout<<" "<<endl;
+    cin>>name>>password;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Password: "<<password<<endl;
+    return 0;
+}
